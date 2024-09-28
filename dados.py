9dd41d79391f9e9c4fc15edd700a37e1f54ff7e2
Gabriel <brancaglioni.gabriel@gmail.com>
2024-09-28 13:40:24 -0300

    com todas as paginas

--- dados.py
+++ dados.py
@@ -4,35 +4,59 @@ import pandas as pd
 import json
 
 
-def single_page():
-    site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
-    try:
-        conectar = requests.get(site)
-        conteudo = conectar.content
-        soup = BeautifulSoup(conteudo, 'html.parser')
-        caminho = soup.find('a', class_="footer_content").get('href')
-        if caminho[0:4] != 'http':
-            link = 'http://vitibrasil.cnpuv.embrapa.br/' + caminho
-        else:
-            link = caminho
-        return link
-    except ValueError:
-        print('Erro pagina invalida')
+def get_link(site):
+    conectar = requests.get(site)
+    conteudo = conectar.content
+    soup = BeautifulSoup(conteudo, 'html.parser')
+    caminho = soup.find('a', class_="footer_content").get('href')
+    if caminho[0:4] != 'http':
+        link = 'http://vitibrasil.cnpuv.embrapa.br/' + caminho
+    else:
+        link = caminho
+    return link
 
 
-def load_transpose_data(link, ano, quantidade):
-    base = pd.read_csv(link, sep=';', encoding = 'utf8')
-    base = base.melt(id_vars=['id', 'control', 'produto'], var_name='ano', value_name='quantidade')
+def load_transpose_data(link, sep, id, ano, quantidade):
+    base = pd.read_csv(link, sep=sep, encoding = 'utf8')
+    names = base.columns
+    vars = []
+    for n in names:
+        if n.isnumeric() == False:
+            vars.append(n)
+    base = base.melt(id_vars=vars, var_name='ano', value_name='quantidade')
     base['ano'] = base['ano'].astype(int)
+    if id is not None:
+      base.query(f'id == {id}', inplace=True)
     if ano is not None:
       base.query(f'ano == {ano}', inplace=True)
     if quantidade is not None:
       base.query(f'quantidade > {quantidade}', inplace=True)
-    base = json.loads(base.to_json(orient='records'))
-    #base = json.dumps(base, ensure_ascii=, indent=4)   
+    base = json.loads(base.to_json(orient='records')) 
     return base
 
 
-
-#for elemento in load_transpose_data(link):
-#    print(elemento)
+def load_transpose_data_double(link, sep, id, ano, quilos, valor):
+    base = pd.read_csv(link, sep=sep, encoding = 'utf8')
+    base.rename(columns={'Id':'id', 'País':'pais'}, inplace=True)
+    principal = ['id','pais']
+    colunas = []
+    for n in base.columns:
+        if n.isnumeric() == True:
+            colunas.append(n)
+    colunas_kg = principal + colunas
+    base_kg = base[colunas_kg].melt(id_vars=principal, var_name='ano', value_name='quilos')
+    base_valor = base.drop(colunas, axis=1)
+    base_valor.columns = base_valor.columns.str.replace('.1', '')
+    base_valor = base_valor.melt(id_vars=principal, var_name='ano', value_name='valor')
+    base = base_kg.merge(base_valor)
+    base['ano'] = base['ano'].astype(int)
+    if id is not None:
+      base.query(f'id == {id}', inplace=True)
+    if ano is not None:
+      base.query(f'ano == {ano}', inplace=True)
+    if quilos is not None:
+      base.query(f'quilos > {quilos}', inplace=True)
+    if valor is not None:
+      base.query(f'valor > {valor}', inplace=True)
+    base = json.loads(base.to_json(orient='records')) 
+    return base
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, make_response, jsonify, Response, render_template, request
-from teste import Dados
 import requests
 from bs4 import BeautifulSoup
 import dados
@@ -9,43 +8,101 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def document():
     return render_template('index.html')
 
 
-@app.route('/{tipo}', methods=['GET'])
-def teste(tipo):
-    if tipo != 'todos':
-        return make_response(
-            jsonify(
-                Dados
-            )
-        )
-    else:
-        return 'Parametro invalido favor consultar a documentação' 
-
-
-@app.route('/<tipo>/<sub_pagina>', methods=['GET'])
-def teste_sub(tipo, sub_pagina):
-    if tipo != 'todos' and sub_pagina != 'teste':
-        return make_response(
-            jsonify(
-                Dados
-            )
-        )
-    else:
-        return 'Parametro invalido favor consultar a documentação' 
-
-
-@app.route('/pagina1', methods=['GET'])
-def valores():
+@app.route('/production', methods=['GET'])
+def production():
+    site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
+    link = dados.get_link(site)
+    id = request.args.get('id', default=None)
     ano = request.args.get('ano', default=None)
     quantidade = request.args.get('quantidade', default=None)
-    link = dados.single_page()
-    resultado = dados.load_transpose_data(link, ano, quantidade)
-    print(type(resultado))
-    resultado = json.dumps(resultado, ensure_ascii=False)
-    print(type(resultado))
+    resultado = dados.load_transpose_data(link, ';', id, ano, quantidade)
+    resultado = json.dumps(resultado, ensure_ascii=False, indent=4)
     return resultado
+
+
+@app.route('/process/<option>', methods=['GET'])
+def process(option):
+    sep = '\t'
+    if option == 'viniferas':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_03'
+        sep = ';'
+    elif option == 'americanas':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_02&opcao=opt_03'
+    elif option == 'mesa':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_03&opcao=opt_03'
+    elif option == 'semclass':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_04&opcao=opt_03'
+    else:
+        return 'Opção invalida, favor consultar a documentação!'
+    link = dados.get_link(site)
+    id = request.args.get('id', default=None)
+    ano = request.args.get('ano', default=None)
+    quantidade = request.args.get('quantidade', default=None)
+    resultado = dados.load_transpose_data(link, sep, id, ano, quantidade)
+    resultado = json.dumps(resultado, ensure_ascii=False, indent=4)
+    return resultado
+
+
+@app.route('/comercial', methods=['GET'])
+def comercial():
+    site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04'
+    link = dados.get_link(site)
+    id = request.args.get('id', default=None)
+    ano = request.args.get('ano', default=None)
+    quantidade = request.args.get('quantidade', default=None)
+    resultado = dados.load_transpose_data(link, ';', id, ano, quantidade)
+    resultado = json.dumps(resultado, ensure_ascii=False, indent=4)
+    return resultado
+
+
+@app.route('/import/<option>', methods=['GET'])
+def importation(option):
+    if option == 'mesa':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_05'
+    elif option == 'espumante':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_02&opcao=opt_05'
+    elif option == 'fresca':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_03&opcao=opt_05'
+    elif option == 'passas':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_04&opcao=opt_05'
+    elif option == 'suco':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_05&opcao=opt_05'
+    else:
+        return 'Opção invalida, favor consultar a documentação!'
+    link = dados.get_link(site)
+    id = request.args.get('id', default=None)
+    ano = request.args.get('ano', default=None)
+    quilos = request.args.get('quilos', default=None)
+    valor = request.args.get('valor', default=None)
+    resultado = dados.load_transpose_data_double(link, ';', id, ano, quilos, valor)
+    resultado = json.dumps(resultado, ensure_ascii=False, indent=4)
+    return resultado
+
+
+@app.route('/export/<option>', methods=['GET'])
+def export(option):
+    if option == 'mesa':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_06'
+    elif option == 'espumante':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_02&opcao=opt_06'
+    elif option == 'fresca':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_03&opcao=opt_06'
+    elif option == 'suco':
+        site = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_04&opcao=opt_06'
+    else:
+        return 'Opção invalida, favor consultar a documentação!'
+    link = dados.get_link(site)
+    id = request.args.get('id', default=None)
+    ano = request.args.get('ano', default=None)
+    quilos = request.args.get('quilos', default=None)
+    valor = request.args.get('valor', default=None)
+    resultado = dados.load_transpose_data_double(link, ';', id, ano, quilos, valor)
+    resultado = json.dumps(resultado, ensure_ascii=False, indent=4)
+    return resultado
+
 
 app.run(port=10000, host='0.0.0.0', debug=True)
